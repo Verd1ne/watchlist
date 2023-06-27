2d7067220b4dfd04ef55beb783a2c93911bd6a58
Verd1ne <vw.anwar@gmail.com>
2023-06-27 18:23:53 +0700

    Data API Fetching in progress

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function page() {
-	return <div>testing</div>;
+async function getData() {
+	const res = await fetch(
+		"https://api.themoviedb.org/3/movie/11?api_key=96a0f6de0d8648b87be78503fc99fb76"
+	);
+	const data = await res.json();
+}
+export default async function page() {
+	// console.log(data);
+	return <div>{data}</div>;
 }
